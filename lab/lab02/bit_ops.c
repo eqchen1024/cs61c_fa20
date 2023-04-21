cb52a7508d00db77ab788bc0505e4531f1436f9e
eqchen1024 <cdysbt@gmail.com>
2023-04-21 11:41:24 +0800

    LAB2 finished

--- lab/lab02/bit_ops.c
+++ lab/lab02/bit_ops.c
@@ -9,7 +9,8 @@ unsigned get_bit(unsigned x,
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns 
     // 0 or 1)
-    return -1;
+    unsigned mask = 1 << n;
+    return (x & mask) >> n;
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
@@ -17,11 +18,15 @@ void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
     // YOUR CODE HERE
+    unsigned to_or = v << n;
+    unsigned to_and = ~(1 << n);
+    * x = (* x & to_and) | to_or;
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
     // YOUR CODE HERE
+    * x = (* x) ^ (1 << n);
 }
 
--- lab/lab02/lfsr.c
+++ lab/lab02/lfsr.c
@@ -6,5 +6,12 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t bit_at_0, bit_at_2, bit_at_3, bit_at_5;
+    bit_at_0 = ((*reg) >> 0) & 1;
+    bit_at_2 = ((*reg) >> 2) & 1;
+    bit_at_3 = ((*reg) >> 3) & 1;
+    bit_at_5 = ((*reg) >> 5) & 1;
+    uint16_t bit_shift_in  = (((bit_at_0 ^ bit_at_2) ^ bit_at_3) ^ bit_at_5);
+    *reg = ((*reg >> 1) & ~(1 << 15)) | (bit_shift_in << 15);
 }
 
